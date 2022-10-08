1450959e0c93a6d6d7f5ba1072e71afe277e1109
ianmeigh <ianmeigh@googlemail.com>
2022-10-08 14:14:06 +0100

    feat: add detail view (retrieve/update) and urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import ProfileListView
+from .views import ProfileDetailView, ProfileListView
 
-urlpatterns = [path("profiles/", ProfileListView.as_view())]
+urlpatterns = [
+    path("profiles/", ProfileListView.as_view()),
+    path("profiles/<int:pk>/", ProfileDetailView.as_view()),
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 
 from .models import Profile
 from .serializers import ProfileSerializer, ProfileSerializerAuthenticated
@@ -9,6 +9,20 @@ class ProfileListView(ListAPIView):
 
     def get_serializer_class(self):
         """Return different serializers based on authentication status
+
+        Authenticated users are able to see profile contact information.
+        """
+        if self.request.user.is_authenticated:
+            return ProfileSerializerAuthenticated
+        else:
+            return ProfileSerializer
+
+
+class ProfileDetailView(RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+
+    def get_serializer_class(self):
+        """Return different serializers based on authentication status.
 
         Authenticated users are able to see profile contact information.
         """
