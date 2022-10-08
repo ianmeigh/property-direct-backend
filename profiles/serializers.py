2e88c426443d0ea5f54973f5381b4754753e1b24
ianmeigh <ianmeigh@googlemail.com>
2022-10-08 15:13:05 +0100

    feat: add auth views and create custom permissions
    Custom permissions classes determine which profile types can be viewed
    and restrict updating (PUT requests) to profile object owners.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,12 +10,17 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
 
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        return self.context["request"].user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             "id",
             "owner",
+            "is_owner",
             "name",
             "description",
             "image",
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from property_direct_api.permissions import IsOwnerOrViewingSeller
 from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 
 from .models import Profile
@@ -5,7 +6,13 @@ from .serializers import ProfileSerializer, ProfileSerializerAuthenticated
 
 
 class ProfileListView(ListAPIView):
-    queryset = Profile.objects.all()
+    """Profile List View
+
+    Only display seller profiles to keep standard user profiles private.
+    Return different Serializer content based on authentication state.
+    """
+
+    queryset = Profile.objects.all().filter(owner__is_seller=True)
 
     def get_serializer_class(self):
         """Return different serializers based on authentication status
@@ -19,7 +26,14 @@ class ProfileListView(ListAPIView):
 
 
 class ProfileDetailView(RetrieveUpdateAPIView):
+    """Profile Detail (Retrieve and Update) View
+
+    Custom permissions class to control profile privacy (and permissions).
+    Return different Serializer content based on authentication state.
+    """
+
     queryset = Profile.objects.all()
+    permission_classes = [IsOwnerOrViewingSeller]
 
     def get_serializer_class(self):
         """Return different serializers based on authentication status.
