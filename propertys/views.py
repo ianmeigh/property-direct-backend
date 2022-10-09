bf4269490edd3691eb69301709696ab5df72c2e6
ianmeigh <ianmeigh@googlemail.com>
2022-10-09 17:28:37 +0100

    feat: add long and lat information to property object
    Longitude and Latitude information fetched from the geocoding API
    (https://api.postcodes.io/) is added to property objects when created or
    updated (the latter only if the postcode has changed).

--- propertys/views.py
+++ propertys/views.py
@@ -7,6 +7,7 @@ from rest_framework.generics import (
 
 from .models import Property
 from .serializers import PropertySerializer
+from .utils import get_postcode_details
 
 
 class PropertyListView(ListAPIView):
@@ -27,7 +28,19 @@ class PropertyCreateView(CreateAPIView):
     queryset = Property.objects.all()
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        """Add extra information before the object is saved (created).
+
+        - Retrieve and add the Longitude and Latitude information for the
+          postcode.
+        - Add an owner before the model object is created.
+        """
+        postcode = serializer.validated_data["postcode"]
+        postcode_details = get_postcode_details(postcode)
+        latitude = postcode_details["latitude"]
+        longitude = postcode_details["longitude"]
+        serializer.save(
+            owner=self.request.user, latitude=latitude, longitude=longitude
+        )
 
 
 class PropertyDetailView(RetrieveUpdateDestroyAPIView):
@@ -36,3 +49,24 @@ class PropertyDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = PropertySerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Property.objects.all()
+
+    def perform_update(self, serializer):
+        """Add extra information before the object is saved (updated).
+
+        - Retrieves the Longitude and Latitude information if the
+          postcode has been updated.
+        """
+        # Get the postcode from the serializer
+        updated_postcode = serializer.validated_data["postcode"]
+
+        # Get the existing property object
+        obj = serializer.instance
+
+        # If the postcode from the serializer doesn't match doesn't match the
+        # property objects current postcode, a change has been made and the
+        # longitude and latitude should be updated.
+        if updated_postcode != obj.postcode:
+            result = get_postcode_details(updated_postcode)
+            obj.latitude = result["latitude"]
+            obj.longitude = result["longitude"]
+        serializer.save()
