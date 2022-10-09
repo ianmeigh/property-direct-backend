843616951ab41f576098d2a2a5c3af9794861bea
ianmeigh <ianmeigh@googlemail.com>
2022-10-09 13:58:38 +0100

    add detail view (retrieve/update/delete), create view and urls

--- propertys/views.py
+++ propertys/views.py
@@ -1,4 +1,9 @@
-from rest_framework.generics import ListAPIView
+from property_direct_api.permissions import IsOwnerOrReadOnly, IsSeller
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 from .models import Property
 from .serializers import PropertySerializer
@@ -9,3 +14,25 @@ class PropertyListView(ListAPIView):
 
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+
+
+class PropertyCreateView(CreateAPIView):
+    """Property Create View
+
+    Custom permissions class to restrict property creation (to only Sellers).
+    """
+
+    serializer_class = PropertySerializer
+    permission_classes = [IsSeller]
+    queryset = Property.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PropertyDetailView(RetrieveUpdateDestroyAPIView):
+    """Property Detail (Retrieve, Update and Destroy) View"""
+
+    serializer_class = PropertySerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Property.objects.all()
