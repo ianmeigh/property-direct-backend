a53657fa8b072a24daf13322e2c42bed15124dc7
ianmeigh <ianmeigh@googlemail.com>
2022-10-10 21:35:29 +0100

    feat: add detail view (retrieve/delete) and urls

--- followers/urls.py
+++ followers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FollowerListView
+from .views import FollowerDetailView, FollowerListView
 
 urlpatterns = [
     path("followers/", FollowerListView.as_view()),
+    path("followers/<int:pk>/", FollowerDetailView.as_view()),
 ]
--- followers/views.py
+++ followers/views.py
@@ -1,11 +1,12 @@
+from property_direct_api.permissions import IsOwnerOrReadOnly
 from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
 from .models import Follower
 from .serializers import FollowerSerializer
 
 
-# CREDIT: Class from Code Institute DRF Tutorial Project with minor adaptations
+# CREDIT: Adapted from the Code Institute DRF Tutorial Project
 # URL: https://github.com/Code-Institute-Solutions/drf-api
 class FollowerListView(ListCreateAPIView):
     """Follower List/Create View
@@ -31,3 +32,25 @@ class FollowerListView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class FollowerDetailView(RetrieveDestroyAPIView):
+    """Follower Detail (Retrieve, Update and Destroy) View
+
+    - Retrieve a Follower object by id and allow the owner to view or delete
+      the object.
+    """
+
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        """Filter the queryset to display only bookmarks owned by the currently
+        authenticated User, to keep bookmarks private.
+        """
+        current_user = self.request.user
+        if current_user.is_anonymous:
+            queryset = Follower.objects.none()
+        else:
+            queryset = Follower.objects.filter(owner=current_user)
+        return queryset
