7ec44a419e4184b259240f74bb4f9eea6d949a08
ianmeigh <ianmeigh@googlemail.com>
2022-10-11 14:06:54 +0100

    feat: add follower id, extra count and ordering fields
    Count fields to provide profile stats.

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Count
 from property_direct_api.permissions import (
     IsProfileOwnerOrViewingSellerProfile,
 )
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 
 from .models import Profile
@@ -14,7 +16,22 @@ class ProfileListView(ListAPIView):
     - Return different Serializer content based on authentication state.
     """
 
-    queryset = Profile.objects.all().filter(owner__is_seller=True)
+    queryset = (
+        Profile.objects.all()
+        .filter(owner__is_seller=True)
+        .annotate(
+            property_count=Count("owner__property", distinct=True),
+            followers_count=Count("owner__followed", distinct=True),
+            following_count=Count("owner__following", distinct=True),
+        )
+        .order_by("-created_at")
+    )
+    filter_backends = [OrderingFilter]
+    ordering_fields = [
+        "property_count",
+        "followers_count",
+        "following_count",
+    ]
 
     def get_serializer_class(self):
         """Return different serializers based on authentication status
@@ -34,7 +51,11 @@ class ProfileDetailView(RetrieveUpdateAPIView):
     - Return different Serializer content based on authentication state.
     """
 
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        property_count=Count("owner__property", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
     permission_classes = [IsProfileOwnerOrViewingSellerProfile]
 
     def get_serializer_class(self):
