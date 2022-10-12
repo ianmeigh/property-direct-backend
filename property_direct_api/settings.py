bc71bff7ccbb87b16bed7a9b9317ef180f5124ff
ianmeigh <ianmeigh@googlemail.com>
2022-10-12 23:26:52 +0100

    feat: add datetime formatting

--- property_direct_api/settings.py
+++ property_direct_api/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%d %b %Y",
     "EXCEPTION_HANDLER": "property_direct_api.exception_handler.custom_exception_handler",
 }
 
