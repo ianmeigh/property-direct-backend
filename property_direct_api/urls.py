d7cf513611ccededd7da7fce3713a2c3b2cf03c8
ianmeigh <ianmeigh@googlemail.com>
2022-10-12 22:44:28 +0100

    fix: add dj-rest-auth log out bug workaround
    dj-rest-auth has a bug that doesn’t allow users to log out. The code in
    this commit was provided by Code Institute as part of their DRF
    Tutorial Project (https://github.com/Code-Institute-Solutions/drf-api)
    to workaround this issue.

--- property_direct_api/urls.py
+++ property_direct_api/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import logout_route
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("dj-rest-auth/logout/", logout_route),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path(
         "dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
